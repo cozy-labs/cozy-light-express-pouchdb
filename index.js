5b3c6d60d68b4beeb0147b612b4e4de2e07da0f8
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-09-15 01:54:37 +0200

    remove wrong line + typo in manifest

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports.configureAppServer = function(app, config, routes, callback) {
-  app.use(auth.connect(basic));
-
   app.use('/db', require('express-pouchdb')(PouchDB));
   callback();
 };
